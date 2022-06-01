d4e2ef8198a327232cc2053348cd49b4bbd88d98
Vansovski <marcelo_vansovski@hotmail.com>
2022-06-01 14:03:03 -0300

    Acoplando banco de dados com Asp.Net WebAPI

--- Back/src/ProEventos.API/Controllers/EventoController.cs
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -1,47 +1,32 @@
 using Microsoft.AspNetCore.Mvc;
 using ProEventos.API.Models;
+using ProEventos.API.Data;
 
 namespace ProEventos.API.Controllers;
+
 
 [ApiController]
 [Route("api/[controller]")]
 public class EventoController : ControllerBase
 {
-    public EventoController()
+    //Injeção de dependecia do DbContext
+    private readonly DataContext _context;
+    public EventoController(DataContext context)
     {
-        
+        _context = context;
     }
 
+    //Retorna todos os Eventos 
     [HttpGet]
     public IEnumerable<Evento> Get()
     {
-       return new Evento[]
-       {new Evento(){
-           EventoId = 1,
-           Tema = "Angular",
-           Local = "Belo Horizonte",
-           Lote = "1",
-           QtdPessoas = 250,
-           DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
-           ImgURL ="Img/URL"
-       },
-       new Evento(){
-           EventoId = 1,
-           Tema = "React",
-           Local = "Belo Horizonte",
-           Lote = "1",
-           QtdPessoas = 50,
-           DataEvento = DateTime.Now.AddDays(5).ToString("dd/MM/yyyy"),
-           ImgURL ="Img/URL"
-       },
-       new Evento(){
-           EventoId = 1,
-           Tema = "Engenharia de Controle e Automação",
-           Local = "Belo Horizonte",
-           Lote = "1",
-           QtdPessoas = 250,
-           DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
-           ImgURL ="Img/URL"
-       }};
+       return _context.Eventos;
+    }
+
+
+    [HttpGet("{id}")]
+    public Evento Get(int id)
+    {
+       return _context.Eventos.FirstOrDefault(evento => evento.EventoId == id);
     }
 }
---
+++ Back/src/ProEventos.API/Data/Migrations/20220601135015_init.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProEventos.API.Data.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Eventos",
+                columns: table => new
+                {
+                    EventoId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Local = table.Column<string>(type: "TEXT", nullable: true),
+                    DataEvento = table.Column<string>(type: "TEXT", nullable: true),
+                    Tema = table.Column<string>(type: "TEXT", nullable: true),
+                    QtdPessoas = table.Column<int>(type: "INTEGER", nullable: false),
+                    Lote = table.Column<string>(type: "TEXT", nullable: true),
+                    ImgURL = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Eventos", x => x.EventoId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Eventos");
+        }
+    }
+}
